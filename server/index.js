cc0297de1be8fb2a41b68007ec47d035eb9c86a5
CarlosIUSalazar <carlosiusalazar@gmail.com>
2020-04-22 20:56:15 +0900

    Half way there
    Co-authored-by: Saggitarie <saggitarie@users.noreply.github.com>

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable prefer-const */
+/* eslint-disable prettier/prettier */
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const { buildSchema } = require("graphql");
@@ -9,22 +11,108 @@ const schema = buildSchema(`
   type Pokemon {
     id: String
     name: String!
+    classification: String
+    types: [String]
+    resistant: [String]
+    weaknesses: [String]
+    weight: Weight
+    height: Height
+    fleeRate: Float
+    evolutionsRequirements: EvolutionsRequirements
+    evolutions: [Evolutions]
+    maxCP: Int
+    maxHP: Int
+    attacks: Attacks
   }
+
+  type Weight {
+    minimum: String
+    maximum: String
+  }
+
+  type Height {
+    minimum: String
+    maximum: String
+  }
+
+  type Evolutions {
+    id: Int
+    name: String
+  }
+
+  type EvolutionsRequirements {
+    amount: Int
+    name: String
+  }
+
+  type Attacks {
+    fast: [Attack]
+    special: [Attack]
+  }
+
+  type Attack {
+    name: String!
+    type: String!
+    damage: Int
+  }
+
   type Query {
     Pokemons: [Pokemon]
     Pokemon(name: String!): Pokemon
+    PokemonById(id: String): Pokemon
+    PokeAttacks(type: String) : [Attack]
+    
   }
 `);
 
-.
+//    PokemonsByAttack(attack: String): Attacks
+// type Mutation {
+//   createMessage(input: MessageInput): Message
+//   updateMessage(id: ID!, input: MessageInput): Message
+// }
 
 // The root provides the resolver functions for each type of query or mutation.
 const root = {
   Pokemons: () => {
     return data.pokemon;
   },
+
   Pokemon: (request) => {
-    return data.pokemon.find((pokemon) => pokemon.name === request.name);
+    const result = data.pokemon.find(
+      (pokemon) => pokemon.name === request.name
+    );
+    //console.log("POKEEEEEEEE",result)
+    return result;
+  },
+
+  PokemonById: (request) => {
+    return data.pokemon.filter((pokemon) => pokemon.id === request.id);
+  },
+
+  PokeAttacks: (request) => {
+    let res;
+    if (request.type === "fast") {
+      res = data.attacks.fast;
+    }
+    if (request.type === "special") {
+      res = data.attacks.special;
+    }
+    console.log("ATAAAAACK", res);
+    return res;
+    //console.log(request);
+    //console.log("data.attacks.fast", data.attacks.special);
+    //const result = data.attack
+    //const result = data.attacks.filter((att) => attacks.Attacks === request.type);
+    //return result;
+  },
+
+  PokemonsByAttack: (request) => {
+    const result = data.pokemon.filter(
+      (pokemon) => pokemon.attacks.fast === request.attack
+    );
+    // [...pokemon.attacks.fast, ...pokemon.attacks.special]
+    //console.log("RESSSUUUUUUULT", result);
+    return result;
   },
 };
 
@@ -32,6 +120,14 @@ const root = {
 const app = express();
 
 /*
+  The only endpoint for your server is `/graphql`- if you are fetching a resource, 
+
+  The only endpoint for your server is `/graphql`- if you are fetching a resource, 
+  you will need to POST your query to that endpoint. Suggestion: check out Apollo-Fetch
+  or Apollo-Client. Note below where the schema and resolvers are connected. Setting graphiql
+  The only endpoint for your server is `/graphql`- if you are fetching a resource, 
+  you will need to POST your query to that endpoint. Suggestion: check out Apollo-Fetch
+  or Apollo-Client. Note below where the schema and resolvers are connected. Setting graphiql
   The only endpoint for your server is `/graphql`- if you are fetching a resource, 
   you will need to POST your query to that endpoint. Suggestion: check out Apollo-Fetch
   or Apollo-Client. Note below where the schema and resolvers are connected. Setting graphiql
